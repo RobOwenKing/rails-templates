5c98ecd65ea4285e32ed7d87b26c727081d0f69f
Robert King <robert.o.king@hotmail.co.uk>
2023-07-09 16:37:27 +0100

    refactor: Alter Rubocop settings curl to pull from my repo

--- devise.rb
+++ devise.rb
@@ -168,7 +168,7 @@ after_bundle do
 
   # Rubocop
   ########################################
-  run "curl -L https://raw.githubusercontent.com/lewagon/rails-templates/master/.rubocop.yml > .rubocop.yml"
+  run "curl -L https://raw.githubusercontent.com/RobOwenKing/rails-templates/master/.rubocop.yml > .rubocop.yml"
 
   # Git
   ########################################
